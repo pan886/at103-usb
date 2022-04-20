538647daae277117dde2312974979540d3e3ed63
pantianwen <1348987953@qq.com>
2022-04-19 18:31:17 -0700

    add handshake

--- project/examples/ADC_DMA/main.c
+++ project/examples/ADC_DMA/main.c
@@ -36,17 +36,18 @@ void main(void)
     /* DMA1 channel1 configuration ----------------------------------------------*/
     DMA_DeInit(DMA_Channel1);
     DMA_StructInit(&DMA_InitStructure);
-    DMA_InitStructure.DMA_PeripheralBaseAddr = ADC1_DR_Address;
-    DMA_InitStructure.DMA_MemoryBaseAddr     = (uint32_t)&ADCConvertedValue;
-    DMA_InitStructure.DMA_DIR                = DMA_DIR_PeripheralSRC;
-    DMA_InitStructure.DMA_BufferSize         = 10;
-    DMA_InitStructure.DMA_PeripheralInc      = DMA_PeripheralInc_Disable;
-    DMA_InitStructure.DMA_MemoryInc          = DMA_MemoryInc_Disable;
-    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-    DMA_InitStructure.DMA_MemoryDataSize     = DMA_MemoryDataSize_HalfWord;
-    DMA_InitStructure.DMA_Mode               = DMA_Mode_Circular;
-    DMA_InitStructure.DMA_Priority           = DMA_Priority_High;
-    DMA_InitStructure.DMA_M2M                = DMA_M2M_Disable;
+    DMA_InitStructure.DMA_PeripheralBaseAddr  = ADC1_DR_Address;
+    DMA_InitStructure.DMA_MemoryBaseAddr      = (uint32_t)&ADCConvertedValue;
+    DMA_InitStructure.DMA_DIR                 = DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_BufferSize          = 1;
+    DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Disable;
+    DMA_InitStructure.DMA_PeripheralDataSize  = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_HalfWord;
+    DMA_InitStructure.DMA_Mode                = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority            = DMA_Priority_High;
+    DMA_InitStructure.DMA_M2M                 = DMA_M2M_Disable;
+    DMA_InitStructure.DMA_PeripheralHandshake = DMA_PeripheralHandshake_ADC1;
     DMA_Init(DMA_Channel1, &DMA_InitStructure);
 
     /* Enable DMA1 channel1 */
