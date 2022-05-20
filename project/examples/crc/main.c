82de1b9c15e72362c8ce874332e9c10260934971
pantianwen <1348987953@qq.com>
2022-05-19 18:41:25 -0700

    2

--- project/examples/crc/main.c
+++ project/examples/crc/main.c
@@ -10,7 +10,7 @@
 #include "at103.h"
 void delay()
 {
-    for (uint16_t i = 0; i < 0xff; i++)
+    for (uint16_t i = 0; i < 0xffff; i++)
         ;
 }
 void main(void)
@@ -19,20 +19,22 @@ void main(void)
     sys_io_init();
     uart_init(UART_BOOT_PORT, UART_PARITY_NONE, UART_STOPBITS_1, UART_DATABITS_8, UART_BOOT_BD);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_CRC, ENABLE);
+    //
+
     CRC->CRCSEED.SEED   = 0;
     CRC->CRC_CTL.CLEAR  = 1;
     CRC->CRC_CTL.INVOUT = 0;
     CRC->CRC_IN         = 0x1;
-
-    //delay();
-
+    //
+    delay();
+    //RCC_AHBPeriphResetCmd(RCC_AHBPeriph_CRC, ENABLE);
     debug("seed = %x\n", *((uint32_t *)(CRC_BASE + 0xFFFC)));
 
     debug("seed2= %x\n", CRC->CRCSEED.SEED);
 
     debug("out2 = %x\n", (*((uint32_t *)(CRC_BASE + 0xFFF4)) & 0xffff));
     debug("out = %x\n", CRC->CRC_OUT.COUT);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_CRC, DISABLE);
+
     // *((uint32_t *)(CRC_BASE + 0xFFFC)) = 0x5A;
     //  *((uint32_t *)(CRC_BASE))          = 0x86;
     // *((uint32_t *)(CRC_BASE + 0xFFF8)) = 0x1;
