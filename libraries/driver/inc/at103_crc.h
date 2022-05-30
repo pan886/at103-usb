155c5e3a41b823897f07eda7ef17d703149a56dd
pantianwen <1348987953@qq.com>
2022-05-29 18:57:24 -0700

    add return value

--- libraries/driver/inc/at103_crc.h
+++ libraries/driver/inc/at103_crc.h
@@ -5,7 +5,6 @@
  * (+) At first, use RCC_AHBPeriphClockCmd(RCC_AHBPeriph_CRC, ENABLE) to enable the CRC clock. \n
  * (+) Then,use CRC_Seed_Config(...) to config the the initial value of crc. \n
  * (+) Finally,use CRC_CalcCRC(...) to get the crc value. \n
- * (+) Note: \n
  * @author pantianwen (pantianwen@163.com)
  * @version 1.0
  * @date 2022-05-17
@@ -31,13 +30,13 @@ void CRC_ResetDR(void);
 /**
   * @brief Computes the 32-bit CRC of a given data word(32-bit).
   * @param[in] Data data word(16-bit) to compute its CRC.
-  * @return uint16_t  
+  * @return uint16_t 16-bit CRC.
   */
 uint16_t CRC_CalcCRC(uint32_t Data);
 
 /**
  * @brief Returns the current CRC value.
- * @return uint16_t  :16-bit CRC
+ * @return uint16_t 16-bit CRC.
  */
 uint16_t CRC_GetCRC(void);
 
@@ -48,21 +47,21 @@ void CRC_Clear_Value(void);
 
 /**
  * @brief CRC input inversion.
- * @param[in] invout: can be 0x1 0r 0x0;
+ * @param[in] invout can be 0x1 0r 0x0;
  */
 void CRC_Input_Control(uint8_t invout);
 
 /**
  * @brief config CRC seed value. 
- * @param[in] value
+ * @param[in] value  the initial value of crc before CRC computation.
  */
 void CRC_Seed_Config(uint16_t value);
 
 /**
  * @brief  Computes the 16-bit CRC of a given buffer of data word(32-bit).
- * @param[in] pBuffer: pointer to the buffer containing the data to be computed.
- * @param[in] BufferLength: length of the buffer to be computed.		
- * @return uint16_t: 16-bit CRC.
+ * @param[in] pBuffer pointer to the buffer containing the data to be computed.
+ * @param[in] BufferLength length of the buffer to be computed.		
+ * @return uint16_t 16-bit CRC.
  */
 uint16_t CRC_CalcBlockCRC(uint32_t pBuffer[], uint32_t BufferLength);
 
